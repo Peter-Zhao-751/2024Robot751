dccea9cbea33ed73e938707f74b3d0d51b4f49c6
Peter <89460630+Peter-Zhao-751@users.noreply.github.com>
2024-01-25 19:38:24 -0800

    removed parallel command wrapper because it is useless

--- src/main/java/frc/robot/commands/ParallelCommandWrapper.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.commands;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
-public class ParallelCommandWrapper extends ParallelCommandGroup{
-    public ParallelCommandWrapper(SequentialCommandGroup action1, SequentialCommandGroup action2){
-        addCommands(action1, action2);
-    }
-}
