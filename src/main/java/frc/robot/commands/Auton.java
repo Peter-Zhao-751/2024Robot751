03893eaa92b149a0c7b4d24e9f419b639aa2cbd9
Peter <89460630+Peter-Zhao-751@users.noreply.github.com>
2024-02-04 14:21:23 -0800

    fixed auton with move

--- src/main/java/frc/robot/commands/Auton.java
+++ src/main/java/frc/robot/commands/Auton.java
@@ -7,15 +7,18 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class Auton extends SequentialCommandGroup {
-    MoveToLocation moveToLocation;
+    
     public Auton(SwerveDrive s_Swerve){
-        MoveToLocation moveToLocation = new MoveToLocation(s_Swerve, new Pose2d(3, 0, new Rotation2d(0)));
+        //simple auton
+        Move moveToLocation = new Move(s_Swerve, new Pose2d(3, 0, new Rotation2d(0)));
         
-        MoveToLocation moveToLocation2 = new MoveToLocation(s_Swerve, new Pose2d(0, 0, new Rotation2d(0)));
+        Move moveToLocation2 = new Move(s_Swerve, new Pose2d(0, 0, new Rotation2d(0)));
         addCommands(moveToLocation, moveToLocation2);
         
     }
     public Auton(SwerveDrive s_Swerve, ArrayList<AutonCommandSegment> path){
-        
+        for (AutonCommandSegment segment : path){
+            addCommands(segment);
+        }
     }
 }
