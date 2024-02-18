7c533a6364bf8995028ea4c20bb45a8c85db9113
Alex <alex@petrys.com>
2024-02-18 15:02:06 -0800

    IDK maybe implemenented stuff
    MotionMagicVelocityVoltage for Shooter Control

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,6 +4,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
@@ -17,15 +21,43 @@ public class ShooterSubsystem extends SubsystemBase implements Component{
     private double maxSpeed;
     private boolean isPriming;
     private double targetSpeed;
+    private final MotionMagicVelocityVoltage motionMagicVelocityVoltage;
 
     public ShooterSubsystem(){
         shooterMotor1 = new TalonFX(Constants.Shooter.leftShooterMotorID);
         shooterMotor2 = new TalonFX(Constants.Shooter.rightShooterMotorID);
+        
+
+        // in init function
+        TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+
+        // set slot 0 gains
+        var slot0Configs = talonFXConfigs.Slot0;
+        slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
+        slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+        slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+        slot0Configs.kP = 0.11; // An error of 1 rps results in 0.11 V output
+        slot0Configs.kI = 0; // no output for integrated error
+        slot0Configs.kD = 0; // no output for error derivative
+
+        // set Motion Magic Velocity settings
+        var motionMagicConfigs = talonFXConfigs.MotionMagic;
+        motionMagicConfigs.MotionMagicAcceleration = 400; // Target acceleration of 400 rps/s (0.25 seconds to max)
+        motionMagicConfigs.MotionMagicJerk = 4000; // Target jerk of 4000 rps/s/s (0.1 seconds)
+
+        shooterMotor1.getConfigurator().apply(talonFXConfigs);
+        shooterMotor2.getConfigurator().apply(talonFXConfigs);
+
+        motionMagicVelocityVoltage = new MotionMagicVelocityVoltage(0);
+
         transferMotor = new CANSparkMax(Constants.Shooter.transferMotorID, MotorType.kBrushless);
     }
     public void shoot(double speed){
-        shooterMotor1.set(speed);
-        shooterMotor2.set(speed);
+        // shooterMotor1.set(speed);
+        // shooterMotor2.set(speed);
+        
+        shooterMotor1.setControl(motionMagicVelocityVoltage.withVelocity(speed));
+        shooterMotor2.setControl(motionMagicVelocityVoltage.withVelocity(speed));
     }
     public void transfer(double speed){
         transferMotor.set(speed);
