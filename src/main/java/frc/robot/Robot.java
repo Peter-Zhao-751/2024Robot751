336ab4c1385084717c6893403feed7d191ca4136
Peter <89460630+Peter-Zhao-751@users.noreply.github.com>
2024-02-25 17:51:27 -0800

    owen's dumb manager

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,12 +25,12 @@ import edu.wpi.first.cscore.CvSource;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.imgcodecs.Imgcodecs;
-
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.SignalLogger;
-
 import frc.robot.subsystems.JsonParser;
-
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -42,6 +42,8 @@ public class Robot extends TimedRobot {
   public static final CTREConfigs ctreConfigs = new CTREConfigs();
 
   private final SendableChooser<File> autonSelector = new SendableChooser<>();
+  private GenericEntry updatePreferencesButton;
+  private GenericEntry resetPreferencesButton;
   private File selectedAuton = null;
   private String base64Image = null;
   private CvSource imageSource;
@@ -69,32 +71,8 @@ public class Robot extends TimedRobot {
 
     // Go through every constant in Constants.java check if the Preference exists.
     // add it if it doesn't. if it does, overwrite it.
-    
-    Field[] fields = Constants.class.getDeclaredFields();
+    updatePreferencesBasedOnConstants(Constants.class, false);
 
-    for (Field field : fields) {
-        // Only log public static fields
-        if (Modifier.isPublic(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) {
-            //
-            System.out.println("Variable name: " + field.getName());
-        }
-    }
-
-    // Logging names of all fields in inner classes of Constants
-    Class<?>[] innerClasses = Constants.class.getDeclaredClasses();
-    
-    for (Class<?> innerClass : innerClasses) {
-        System.out.println("Inner class: " + innerClass.getSimpleName());
-        fields = innerClass.getDeclaredFields();
-        for (Field field : fields) {
-            // Only log public static fields
-            if (Modifier.isPublic(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) {
-                System.out.println("Variable name: " + field.getName());
-            }
-        }
-    }
-
-    
     // robot container
     m_robotContainer = new RobotContainer();
     SignalLogger.setPath("/media/sda1/");
@@ -197,10 +175,115 @@ public class Robot extends TimedRobot {
     }
   }
 
+  /**
+   * This function will go through the provided class (the Constants class) and update each
+   * preference based on the value of the constant. If the preference does not exist, it will be
+   * created. If it does exist, the Constant will be updated to match the preference.
+   */
+  private void updatePreferencesBasedOnConstants(Class<?> clazz, boolean reset) {
+      Field[] fields = clazz.getDeclaredFields();
+      for (Field field : fields) {
+          if (Modifier.isPublic(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) {
+              if (reset) resetPreferenceToConstant(field);
+              else updateOrRetrievePreference(field);
+          }
+      }
+
+      // Recursively handle inner classes
+      Class<?>[] innerClasses = clazz.getDeclaredClasses();
+      for (Class<?> innerClass : innerClasses) {
+          updatePreferencesBasedOnConstants(innerClass, reset);
+      }
+  }
+
+  /**
+   * This function will update the preference based on the value of the field, or retrieve the
+   * preference and update the field's value based on the preference.
+   */
+  private void updateOrRetrievePreference(Field field) {
+      try {
+          // Ensure the field is accessible
+          field.setAccessible(true);
+          // Check if a Preference exists for this field
+          if (!Preferences.containsKey(field.getName())) {
+              switch (field.getType().getName()) {
+                case "int":
+                    Preferences.setInt(field.getName(), field.getInt(null));
+                    break;
+                case "double":
+                    Preferences.setDouble(field.getName(), field.getDouble(null));
+                    break;
+                case "boolean":
+                    Preferences.setBoolean(field.getName(), field.getBoolean(null));
+                    break;
+                case "java.lang.String":
+                    Preferences.setString(field.getName(), (String) field.get(null));
+                    break;
+              }
+          } else {
+              // Preference exists, update field's value from Preference
+              switch (field.getType().getName()) {
+                case "int":
+                    field.setInt(null, Preferences.getInt(field.getName(), field.getInt(null)));
+                    break;
+                case "double":
+                    field.setDouble(null, Preferences.getDouble(field.getName(), field.getDouble(null)));
+                    break;
+                case "boolean":
+                    field.setBoolean(null, Preferences.getBoolean(field.getName(), field.getBoolean(null)));
+                    break;
+                case "java.lang.String":
+                    field.set(null, Preferences.getString(field.getName(), (String) field.get(null)));
+                    break;
+              }
+          }
+      } catch (IllegalArgumentException | IllegalAccessException e) {
+          e.printStackTrace();
+      }
+  }
+
+  /**
+   * This function will reset the preference based on the value of the field, or retrieve the
+   * preference and reset the field's value based on the preference.
+   */
+  private void resetPreferenceToConstant(Field field) {
+      try {
+          // Ensure the field is accessible
+          field.setAccessible(true);
+          // Check if a Preference exists for this field
+          if (Preferences.containsKey(field.getName())) {
+              // Preference exists, update preferences's value from field
+              switch (field.getType().getName()) {
+                case "int":
+                    Preferences.setInt(field.getName(), field.getInt(null));
+                    break;
+                case "double":
+                    Preferences.setDouble(field.getName(), field.getDouble(null));
+                    break;
+                case "boolean":
+                    Preferences.setBoolean(field.getName(), field.getBoolean(null));
+                    break;
+                case "java.lang.String":
+                    Preferences.setString(field.getName(), (String) field.get(null));
+                    break;
+              }
+          }
+      } catch (IllegalArgumentException | IllegalAccessException e) {
+          e.printStackTrace();
+      }
+  }
+
   private void updateTelemetry(){
     SmartDashboard.putBoolean("Current Manager Over Nominal", CurrentManager.isOverNominal());
     SmartDashboard.putBoolean("Current Manager Over Peak", CurrentManager.isOverMax());
-
+    if (updatePreferencesButton.getBoolean(false)) {
+        updatePreferencesBasedOnConstants(Constants.class, false);
+        updatePreferencesButton.setBoolean(false); // Reset the toggle button
+    }
+    if (resetPreferencesButton.getBoolean(false)) {
+        updatePreferencesBasedOnConstants(Constants.class, true);
+        resetPreferencesButton.setBoolean(false); // Reset the toggle button
+    }
     updatePathPreview();
   }
 
@@ -237,5 +320,15 @@ public class Robot extends TimedRobot {
       .withWidget("Combo Box Chooser");
 
     SmartDashboard.putString("Current Mode", currentMode.toString());
+
+    updatePreferencesButton = Shuffleboard.getTab("Preferences")
+    .add("Update Preferences", false)
+    .withWidget(BuiltInWidgets.kToggleButton)
+    .getEntry();
+
+    resetPreferencesButton = Shuffleboard.getTab("Preferences")
+    .add("Reset Preferences", false)
+    .withWidget(BuiltInWidgets.kToggleButton)
+    .getEntry();
   }
 }
