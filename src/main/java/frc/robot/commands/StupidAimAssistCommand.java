93913347a8e546e53524502699b42bfeaa7ca88c
Peter <89460630+Peter-Zhao-751@users.noreply.github.com>
2024-03-21 22:03:45 -0700

    somethin

---
+++ src/main/java/frc/robot/commands/StupidAimAssistCommand.java
@@ -0,0 +1,75 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.Constants.FieldConstants;
+import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.utility.StateMachine;
+
+
+public class StupidAimAssistCommand extends Command {
+	private final LimelightSubsystem limelight;
+	private final SwerveSubsystem s_Swerve;
+	private MoveCommand moveCommand;
+
+	public StupidAimAssistCommand() {
+		this.limelight = LimelightSubsystem.getInstance();
+		this.s_Swerve = SwerveSubsystem.getInstance();
+		addRequirements(s_Swerve);
+	}
+
+	@Override
+	public void initialize() {
+		StateMachine.setState(StateMachine.State.Aimbot);
+		Pose2d pose = limelight.getPose();
+
+		// if there is no target, then cancel the command
+		if (pose == null) {
+			cancel();
+			return;
+		}
+
+		// if the 2 blue field element is closer than the 2 red, then use the blue field elements, otherwise use the red field elements
+		FieldConstants.FieldElements redSpeaker = FieldConstants.red[1];
+		FieldConstants.FieldElements blueSpeaker = FieldConstants.blue[1];
+
+		FieldConstants.FieldElements closestSpeaker;
+
+		if (getDistance(redSpeaker.x, redSpeaker.y, pose.getX(), pose.getY()) < getDistance(blueSpeaker.x, blueSpeaker.y, pose.getX(), pose.getY())) {
+			closestSpeaker = redSpeaker;
+		} else {
+			closestSpeaker = blueSpeaker;
+		}
+
+		double angle = Math.atan2(closestSpeaker.y - pose.getY(), closestSpeaker.x - pose.getX());
+
+		angle = Math.toDegrees(angle);
+		angle = (angle + 360) % 360;
+
+		moveCommand = new MoveCommand(new Pose2d(pose.getX(), pose.getY(), new Rotation2d(angle)));
+		moveCommand.initialize();
+	}
+
+	private double getDistance(double x1, double y1, double x2, double y2) {
+		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+	}
+
+	@Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        StateMachine.setState(StateMachine.State.Idle);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return moveCommand == null || moveCommand.isFinished();
+    }
+}
